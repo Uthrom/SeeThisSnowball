12375719cd88c9d4d4bf86d984cd8a6796f88fc7
Uthrom <>
2019-11-18 00:44:19 -0600

    Updates to try to only catch specific damage

--- lib/event_callbacks.lua
+++ lib/event_callbacks.lua
@@ -5,7 +5,7 @@ local function teleport(player, destination, r)
     local pos = { math.random(destination.x - r, destination.x + r), math.random(destination.y - r, destination.y + r) }
     local dst = surface.find_non_colliding_position("character", pos, 8, 2)
 
-    log("Old pos: " .. serpent.block(destination) .. ", new pos: " .. serpent.block(dst))
+--    log("Old pos: " .. serpent.block(destination) .. ", new pos: " .. serpent.block(dst))
 
     if not dst then
       player.teleport(destination, surface)
@@ -21,8 +21,9 @@ function event_callbacks.on_entity_damaged (event)
     return
   end
 
-  if event.entity.type == "character" and event.cause.type == "character" and event.entity.player.valid then
-      teleport(event.entity.player, event.entity.position, 50)
+  if event.damage_type.name == "impact" and event.entity.type == "character" and event.cause.type == "character" and event.entity.player.valid then
+    teleport(event.entity.player, event.entity.position, 50)
+    event.entity.damage( -1, event.entity.force, "impact")
   end
 end 
 
