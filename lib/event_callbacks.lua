2bee6285a1874cf0d3af0017cc8d5cb7bb1bbc27
Uthrom <>
2019-12-01 22:25:34 -0600

    Cleaned up debug

--- lib/event_callbacks.lua
+++ lib/event_callbacks.lua
@@ -18,22 +18,16 @@ function event_callbacks.on_entity_damaged (event)
   end
 
   if event.damage_type.name == "snowball" then
---    log("Snowball damage")
     local s_dist = settings.global['snowball-tp-distance'].value
     local s_vehicles = settings.global['snowball-allow-vehicles'].value
     local s_biters   = settings.global['snowball-allow-biters'].value
     local s_entities = settings.global['snowball-allow-entities'].value
 
---    log("Before: " .. event.entity.type)
-
     if event.entity.type == "character" and event.entity.player.is_player() then
---	log("Character")
         teleport(event.entity, event.entity.position, s_dist)
     elseif event.entity.type == "car" and s_vehicles == true then
---	log("Car")
         teleport(event.entity, event.entity.position, s_dist)
     elseif event.entity.type == "unit" and s_biters == true then
---	log("Unit")
         teleport(event.entity, event.entity.position, s_dist)
 --    else
 --      log(serpent.block(event.entity.type))
@@ -47,7 +41,6 @@ function event_callbacks.on_trigger_created_entity (event)
   if not event.entity.valid then
     return
   else 
---    log(serpent.block(event))
     event.entity.destroy()
   end
 end
