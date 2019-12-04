9f1e767079e324832da9b68612b584940938bf57
Uthrom <>
2019-12-04 00:22:23 -0600

    Moved on_even initialization

--- control.lua
+++ control.lua
@@ -1,8 +1,3 @@
 event_callbacks = require("lib.event_callbacks")
 
-script.on_event( defines.events.on_entity_damaged,
-                 event_callbacks.on_entity_damaged )
-
-script.on_event( defines.events.on_trigger_created_entity,
-                 event_callbacks.on_trigger_created_entity )
-
+event_callbacks.Init()
