cb0341fb2dedd4c687653f107754a66b2b30c060
Uthrom <>
2019-12-06 00:32:32 -0600

    Handler updates and rename

---
+++ lib/events.lua
@@ -0,0 +1,57 @@
+local Events = {}
+
+local function teleport(entity, dest, r)
+  local surface = entity.surface
+  local pos = { math.random(dest.x - r, dest.x + r), math.random(dest.y - r, dest.y + r) }
+  local dst = surface.find_non_colliding_position("character", pos, 8, 2)
+
+  if not dst then
+    entity.teleport(dest)
+  else
+    entity.teleport(dst)
+  end
+end
+
+function Events.Init (e)
+  local damage_filter = {{filter='type', type='character'}}
+
+  if global.Mod.SnowballAllowVehicles == true then
+    table.insert(damage_filter, {filter='type', type='car'})
+  end
+
+  if global.Mod.SnowballAllowBiters == true then
+    table.insert(damage_filter, {filter='type', type='unit'})
+   end
+
+  script.on_event( defines.events.on_entity_damaged, 
+                   Events._damaged_entity, 
+                   damage_filter )
+
+  script.on_event( defines.events.on_trigger_created_entity,
+                   Events.on_trigger_created_entity )
+end
+
+function Events._damaged_entity (event)
+  if not event.entity.valid then
+    return
+  end
+
+  if not event.damage_type.name == "snowball" then
+    return
+  end
+
+  local s_dist = settings.global['snowball-tp-distance'].value
+  teleport(event.entity, event.entity.position, s_dist)
+  event.entity.damage( event.final_damage_amount * -1, event.entity.force, "snowball")
+
+end
+
+function Events.on_trigger_created_entity (event)
+  if not event.entity.valid then
+    return
+  else 
+    event.entity.destroy()
+  end
+end
+
+return Events
